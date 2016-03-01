24bb58a9832431244c5b37c8ac55992dc52d3c03
unknown <v.birembaut@J001-0133-07.etudiant.rubika-edu.com.local>
2016-03-01 19:28:45 +0100

    loultest

---
+++ Assets/Resources/Scripts/DiceRotation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceRotation : MonoBehaviour {
+
+    Rigidbody rb;
+    Vector3 position;
+    public float force = 300f;
+    bool canDraw = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 direction = Vector3.up * force;
+            rb.AddForce(direction);
+            transform.rotation = Random.rotation;
+            //StartCoroutine("Test");
+        }
+        Debug.Log("position X : " + (transform.rotation.x * Mathf.Rad2Deg) + ",  position Z : " + (transform.rotation.y* Mathf.Rad2Deg ));
+    }
+
+    IEnumerator Test()
+    {
+        if (transform.position == position)
+        {
+            Debug.Log("position X : " + (int)(transform.rotation.x) + ",  position Z : " + (int)(transform.rotation.z));
+            yield return null;
+        }
+        else
+        {
+            position = transform.position;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
