845f043fe06d91ec97c6979c45375409b22bd8fa
Cédric Brun <brun.cedric2233@gmail.com>
2016-03-01 14:26:51 +0100

    Ajout classe objects

---
+++ Assets/Resources/Scripts/Objects/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour
+{
+    private string cardName;
+    private int energy;
+
+    public Card(int energy, string name)
+    {
+        this.energy = energy;
+        cardName = name;
+    }
+
+    public string getName()
+    {
+        return cardName;
+    }
+
+    public int getEnergy()
+    {
+        return energy;
+    }
+}
---
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    private int score;
+    private int mana;
+    private int multiplier;
+    private List<Card> deck;
+    private List<Card> hand;
+    private List<Card> graveyard;
+
+    public Player()
+    {
+        score = 0;
+        mana = 0;
+        multiplier = 0;
+        deck = new List<Card>();
+        hand = new List<Card>();
+        graveyard = new List<Card>();
+    }
+
+    public void draw()
+    {
+        int nb = (int)Random.Range(0, deck.Count-1);
+        hand.Add(deck[nb]);
+        deck.Remove(deck[nb]);
+    }
+}
