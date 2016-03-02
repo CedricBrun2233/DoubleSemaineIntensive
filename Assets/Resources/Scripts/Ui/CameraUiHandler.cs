3b14c19c563803b41710286e921d7b56acd4ab03
valgoun <valgoun@gmail.com>
2016-03-02 14:35:12 +0100

    Start Frame and Main Menu Fram
    Start frame with animations and interactions.
    Menu Frame with animations and basic interactions.
    TODO list :
    - Menu frame links to other frame
    - better transitions between start frame and menu frame

---
+++ Assets/Resources/Scripts/Ui/CameraUiHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+[ExecuteInEditMode]
+public class CameraUiHandler : MonoBehaviour
+{
+	[Header ("PointContainer")]
+	[SerializeField]
+	private Transform focusPoints;
+	[SerializeField]
+	private Transform Points;
+	[Header ("Debug")]
+	[SerializeField]
+	private Transform activeFocusPoint;
+	[SerializeField]
+	private Transform activePoint;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!Application.isPlaying) {
+			transform.LookAt (activeFocusPoint, Vector3.up);
+
+			transform.position = activePoint.position;
+
+		}
+		Debug.DrawRay (transform.position, transform.forward * 5, Color.red);
+	}
+
+	public void goToFrame (string name)
+	{
+
+		activeFocusPoint = focusPoints.FindChild (name);
+		activePoint = Points.FindChild (name);
+		transform.DOLookAt (activeFocusPoint.position, 0.75f, AxisConstraint.None, Vector3.up).SetEase (Ease.InOutSine).OnComplete (() => {
+			transform.DOMove (activePoint.position, 0.75f).SetEase (Ease.InOutSine).OnUpdate (() => {
+				transform.LookAt (activeFocusPoint, Vector3.up);
+			});
+		});
+
+	}
+}
---
+++ Assets/Resources/Scripts/Ui/MainMenuHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenuHandler : MonoBehaviour
+{
+	[SerializeField]
+	private EventSystem system;
+	[SerializeField]
+	private GameObject firstSelected;
+
+	private CanvasGroup grp;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		grp = GetComponent<CanvasGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Appear ()
+	{
+		grp.DOFade (1, 1).SetEase (Ease.InOutSine);
+		system.SetSelectedGameObject (firstSelected);
+	}
+
+	public void ButtonSelect (GameObject button)
+	{
+		button.transform.DOLocalMoveZ (-0.5f, 0.5f);
+	}
+
+	public void ButtonDeselect (GameObject button)
+	{
+		button.transform.DOLocalMoveZ (0f, 0.5f);
+	}
+}
