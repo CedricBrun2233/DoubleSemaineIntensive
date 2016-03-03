729b15cf504ec2b25bded20498fdc26c6fbbcce0
Cédric Brun <brun.cedric2233@gmail.com>
2016-03-03 10:15:15 +0100

    Preparation pour merge

--- Assets/Resources/Scripts/Player/Player.cs
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -7,30 +7,55 @@ public class Player
     private int score;
     private int mana;
     private int multiplier;
+    [HideInInspector]
+    public Card selected;
     public Dice[] dices;
-    private List<Card> deck;
     private List<Card> hand;
-    private List<Card> graveyard;
 
     public Player()
     {
         score = 0;
         mana = 0;
         multiplier = 1;
+        selected = null;
         dices = new Dice[3];
-        deck = new List<Card>();
+        for(int i = 0; i < 3; i++)
+        {
+            dices[i] = new Dice();
+        }
         hand = new List<Card>();
-        graveyard = new List<Card>();
     }
 
     public void AddMana(int value)
     {
         mana += value;
     }
+    public void RollDice()
+    {
+        foreach (Dice dice in dices)
+        {
+            dice.RollDice();
+        }
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void AddCardInHand(Card card)
+    {
+        hand.Add(card);
+    }
 
     public void EndOfTurn()
     {
         multiplier = 1;
         mana = 0;
+    }
+
+    public int getHandSize()
+    {
+        return hand.Count;
     }
 }
